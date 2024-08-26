ccf5acc0f215cd37e4f877c123c37148d8682323
Kaitlynesch17 <Kaitlyn.esch@gmail.com>
2024-08-26 09:29:20 -0500

    React 3 exercise

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Crochet</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Crochet is uses only one hook, unlike knitting that uses two.</li>
+            <li>The easiest stitiches are a single crochet and a double crochet.</li>
+            <li>You can crochet with any kind of yarn you want.</li>
          </ol>
       </div>
    );
